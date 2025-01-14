b222d7a47dcec4d80563c5570834a874116a8307
will <41879468+willgithubusername@users.noreply.github.com>
2025-01-14 15:56:01 -0500

    Add files via upload

---
+++ Invoke.ps1
@@ -0,0 +1,11 @@
+$path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\TaskbarDeveloperSettings"
+      $name = "TaskbarEndTask"
+      $value = 1
+
+      # Ensure the registry key exists
+      if (-not (Test-Path $path)) {
+        New-Item -Path $path -Force | Out-Null
+      }
+
+      # Set the property, creating it if it doesn't exist
+      New-ItemProperty -Path $path -Name $name -PropertyType DWord -Value $value -Force | Out-Null
